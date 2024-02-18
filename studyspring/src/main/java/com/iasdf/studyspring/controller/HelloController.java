10783580bccce7618938d82ba742ee05a13fa535
jhh9232 <ghkd01212@gmail.com>
2024-02-18 13:55:01 +0000

    controller 추가

---
+++ studyspring/src/main/java/com/iasdf/studyspring/controller/HelloController.java
@@ -0,0 +1,48 @@
+package com.iasdf.studyspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        @Getter
+        @Setter
+        private String name;
+    }
+}
