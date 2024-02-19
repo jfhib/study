4761d95040e67b3d680db363fef29d4da23109f1
jhh9232 <ghkd01212@gmail.com>
2024-02-19 13:45:51 +0000

    member 코드 추가

---
+++ studyspring/src/main/java/com/iasdf/studyspring/domain/Member.java
@@ -0,0 +1,17 @@
+package com.iasdf.studyspring.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Member {
+
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
+    private String name;
+
+
+}
